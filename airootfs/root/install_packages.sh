5773a15bd42477f97385b9bab6101a1f3924233a
Anes Belfodil <ans.belfodil@gmail.com>
2019-07-15 18:46:03 -0400

    Add package installation script

--- airootfs/root/install_packages.sh
+++ airootfs/root/install_packages.sh
@@ -2,3 +2,12 @@
 
 ROOT="/mnt"
 CHROOT="arch-chroot $ROOT"
+
+INSTALL=sudo -u nobody trizen -Syu --needed --noedit
+
+cp -r /root/packages $ROOT/packages
+
+$CHROOT 'bash -c "for file in /packages/*.pkg; do $INSTALL $(cat $file); done"'
+
+rm -rf $ROOT/packages
+
