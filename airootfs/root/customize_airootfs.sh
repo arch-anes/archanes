3b9bb1fb7f6ede683afc0dc3b2b42f4d9df3f3a7
Anes Belfodil <ans.belfodil@gmail.com>
2019-07-15 17:26:39 -0400

    Link nvim to vim

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -21,3 +21,6 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
+
+ln -sf /bin/nvim /bin/vim
+
