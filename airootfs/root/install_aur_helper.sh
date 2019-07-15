d0ee119634bf52862ff9715509c859a0059bc608
Anes Belfodil <ans.belfodil@gmail.com>
2019-07-15 18:31:47 -0400

    Add aur helper installation script

---
+++ airootfs/root/install_aur_helper.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+ROOT="/mnt"
+CHROOT="arch-chroot $ROOT"
+
+BUILD=/home/build
+
+$CHROOT mkdir -p $BUILD
+$CHROOT chgrp nobody $BUILD
+$CHROOT chmod g+ws $BUILD
+$CHROOT setfacl -m u::rwx,g::rwx $BUILD
+$CHROOT setfacl -d --set u::rwx,g::rwx,o::- $BUILD
+
+TRIZEN=$ROOT$BUILD/trizen
+git clone https://aur.archlinux.org/trizen.git $TRIZEN
+$CHROOT pushd $TRIZEN && sudo -u nobody makepkg
+
