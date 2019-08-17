7fabbaecc59c11745a11e7dbfab6ab834024b32e
Anes Belfodil <ans.belfodil@gmail.com>
2019-08-17 09:54:55 -0400

    Add more packages to graphics drivers installation script

--- airootfs/root/scripts/install_graphics_drivers.sh
+++ airootfs/root/scripts/install_graphics_drivers.sh
@@ -5,10 +5,11 @@ echo "Installing graphics drivers"
 VGA=$(lspci | grep VGA)
 case $VGA in
   *AMD*)
-    $CHROOT $INSTALL_CMD xf86-video-amdgpu mesa lib32-mesa
+    $CHROOT $INSTALL_CMD xf86-video-amdgpu mesa lib32-mesa amdgpu-fan
+    $CHROOT systemctl enable amdgpu-fan.service
     ;;
   *NVIDIA*)
-    $CHROOT $INSTALL_CMD nvidia nvidia-utils lib32-nvidia-utils
+    $CHROOT $INSTALL_CMD nvidia nvidia-utils lib32-nvidia-utils nvidia-settings
     ;;
   *Intel*)
     $CHROOT $INSTALL_CMD xf86-video-intel mesa lib32-mesa
