f049ec53b518fc0276413805752e4ab7dbc05187
Anes Belfodil <ans.belfodil@gmail.com>
2019-08-17 07:52:14 -0400

    Setup system clock

--- airootfs/root/scripts/set_locale.sh
+++ airootfs/root/scripts/set_locale.sh
@@ -5,3 +5,7 @@ echo "Setting locale to 'en_US.UTF-8'"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' $ROOT_DIR/etc/locale.gen
 $CHROOT locale-gen
 echo "LANG=en_US.UTF-8" > $ROOT_DIR/etc/locale.conf
+
+echo "Setting system clock"
+$CHROOT timedatectl set-ntp true
+$CHROOT hwclock --systohc
