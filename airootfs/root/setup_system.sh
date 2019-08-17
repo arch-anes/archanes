3ef1e4c0d844b95dd7674ed2e4839c855eabcca5
Anes Belfodil <ans.belfodil@gmail.com>
2019-08-17 07:50:27 -0400

    Install packages at the end

--- airootfs/root/setup_system.sh
+++ airootfs/root/setup_system.sh
@@ -15,7 +15,7 @@ $SCRIPTS_DIR/rankmirrors.sh
 $SCRIPTS_DIR/install_base.sh
 $SCRIPTS_DIR/set_locale.sh
 $SCRIPTS_DIR/set_hostname.sh
-$SCRIPTS_DIR/install_aur_helper.sh
-$SCRIPTS_DIR/install_packages.sh
 $SCRIPTS_DIR/add_user.sh
 $SCRIPTS_DIR/set_root_pw.sh
+$SCRIPTS_DIR/install_aur_helper.sh
+$SCRIPTS_DIR/install_packages.sh
