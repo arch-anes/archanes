63880b069ce6ec8c19a4fd1399730520d27d0751
Anes Belfodil <ans.belfodil@gmail.com>
2019-08-18 03:31:01 -0400

    Set locale right after installing base packages

--- airootfs/root/setup_system.sh
+++ airootfs/root/setup_system.sh
@@ -15,6 +15,7 @@ $SCRIPTS_DIR/partition_drive.sh
 
 $SCRIPTS_DIR/rankmirrors.sh
 $SCRIPTS_DIR/install_base.sh
+$SCRIPTS_DIR/set_locale.sh
 
 $SCRIPTS_DIR/add_user.sh
 $SCRIPTS_DIR/install_aur_helper.sh
@@ -28,6 +29,6 @@ function install_pkgs() {
 $SCRIPTS_DIR/install_packages.sh
 $SCRIPTS_DIR/install_boot.sh
 $SCRIPTS_DIR/install_graphics_drivers.sh
-$SCRIPTS_DIR/set_locale.sh
+
 $SCRIPTS_DIR/set_hostname.sh
 $SCRIPTS_DIR/set_root_pw.sh
