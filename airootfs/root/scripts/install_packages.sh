542c379a6d546f8738d3266dafffa22ef6f8ff87
Anes Belfodil <ans.belfodil@gmail.com>
2019-08-18 04:28:12 -0400

    Simplify install_packages script

--- airootfs/root/scripts/install_packages.sh
+++ airootfs/root/scripts/install_packages.sh
@@ -5,7 +5,7 @@ source /root/helpers.sh
 PKG_DIR=/root/packages
 
 echo "Installaing explicit packages"
-for file in $PKG_DIR/*.explicit.pkg; do install_pkgs $(cat $file); done
+install_pkgs $(cat $PKG_DIR/*.explicit.pkg)
 
 echo "Installaing dependency packages"
-for file in $PKG_DIR/*.dependency.pkg; do install_pkgs --asdep $(cat $file); done
+install_pkgs --asdep $(cat $PKG_DIR/*.dependency.pkg)
