725e5c3ef58acf1fff7254eb5d661438641c0a10
Anes Belfodil <ans.belfodil@gmail.com>
2019-12-15 15:35:40 -0500

    SSH: Permit empty password

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -12,6 +12,7 @@ cp -aT /etc/skel/ /root/
 chmod 700 /root
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i 's/#\(PermitEmptyPasswords \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
